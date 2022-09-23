e606174604a564b0dfc6acfd924065b0f9fe5eca
TehTemM@gmail.com <TehTemM@gmail.com>
2022-09-23 14:41:21 +1000

    Кнопка3?

--- Window1.xaml.cs
+++ Window1.xaml.cs
@@ -10,34 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WFP1FILIPPOV1
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Привет, группа ИСИП");
-        }
-
-        private void Win1_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Win2_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
